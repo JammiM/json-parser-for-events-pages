a5b5b78546465fd6598dff9705e90da3c68a026f
James Morrissey <jammim19@gmail.com>
2025-03-20 17:56:22 +0000

    Outlined some basic tests

--- index.test.js
+++ index.test.js
@@ -16,3 +16,13 @@ describe("Check if the folder exists", () => {
     expect(doesTheFolderExist(folderPath)).toBe(true);
   });
 });
+
+describe("Check if the folder contains a valid JSON file", () => {});
+
+describe("Check if the JSON file has atleast one event", () => {});
+
+describe("Check if the event(node) has a id, title, eventUrl, and description", () => {});
+
+describe("Check if the event's has a name, address and city", () => {});
+
+describe("Check if the event(node) has a dateTime and endTime", () => {});

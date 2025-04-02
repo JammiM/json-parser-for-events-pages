7ace6f27c4fba2b35705bb1e0a46231aee1aaa64
James Morrissey <jammim19@gmail.com>
2025-04-02 20:10:50 +0100

    Added a unit test to check if a event has all the required props

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { existsSync, readFileSync, readdirSync, readFile } from "fs";
+import { existsSync, readFileSync, readdirSync } from "fs";
 
 function doesThePathExist(path) {
   return existsSync(path);
@@ -39,9 +39,38 @@ function doesItHaveOneEvent(jsonFile) {
   }
 }
 
+function doesEventHaveAllProps(evObjectStr) {
+  if (!evObjectStr) return;
+
+  // Replaces the '\n\n' within the 'description' as it's caused problems when parsing to JSON
+  const cleanedevObjectStr = evObjectStr
+    .replace(/\n/g, "\\n")
+    .replace(/\r/g, "\\r")
+    .replace(/\t/g, "\\t");
+
+  const eventObject = JSON.parse(cleanedevObjectStr);
+
+  const requiredProps = [
+    "id",
+    "title",
+    "description",
+    "eventUrl",
+    "dateTime",
+    "endTime",
+    "venue",
+  ];
+
+  const hasAllElements = requiredProps.every((elem) =>
+    Object.keys(eventObject).includes(elem)
+  );
+
+  return hasAllElements;
+}
+
 export {
   doesThePathExist,
   containsJsonFile,
   isValidJsonFile,
   doesItHaveOneEvent,
+  doesEventHaveAllProps,
 };
--- index.test.js
+++ index.test.js
@@ -13,6 +13,7 @@ import {
   isValidJsonFile,
   containsJsonFile,
   doesItHaveOneEvent,
+  doesEventHaveAllProps,
 } from "./index";
 
 describe("Check if the folder exists contains a valid JSON file", () => {
@@ -54,6 +55,8 @@ describe("Check if the folder exists contains a valid JSON file", () => {
 describe("Check if the JSON file has the required properties", () => {
   const folderPath = "./test-json-files";
   const jsonFile = join(folderPath, "singleEventTest.json");
+  const jsonString =
+    '{"id": "294055134", "title": "Cork|Sec 121","eventUrl": "https://www.meetup.com/corksec/events/294055134/", "description": "1-2 talks  [http://corksec.com/.](http://corksec.com/) are.\n\nEmail us at DefconCork@gmail.com.\n\nDoors open at 19:00.\n\n**~~TALK 1: Gerard Barrett~~**\n~~Gerard.~~\n\n**~~TALK 2: Business ~~**\n~~A real life case study .~~","venue": {    "id": "27405729",    "name": "Aye",    "address": "9 Anglesea Street","city": "Cork",    "state": "CO",    "country": "ie",    "__typename": "Venue"},"dateTime": "2023-07-11T19:00:00+01:00","createdTime": "2023-06-08T01:32:29-04:00","endTime": "2023-07-11T21:00:00+01:00","__typename": "Event"}';
 
   beforeAll(() => {
     if (!existsSync(folderPath)) {
@@ -76,10 +79,11 @@ describe("Check if the JSON file has the required properties", () => {
   test("Check if the JSON file has atleast one event", () => {
     expect(doesItHaveOneEvent(jsonFile)).toBe(true);
   });
+
+  test("Check if the event has the requied properties", () => {
+    // id, title, eventUrl, description, dateTime and endTime and
+    // venue v name, address and city
+    // dateTime and endTime
+    expect(doesEventHaveAllProps(jsonString)).toBe(true);
+  });
 });
-
-describe("Check if the event(node) has a id, title, eventUrl, and description", () => {});
-
-describe("Check if the event's has a name, address and city", () => {});
-
-describe("Check if the event(node) has a dateTime and endTime", () => {});
